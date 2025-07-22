73d62b44d63aedd57dee5b1bab73301f7d11cd23
Adwait Patel <145041038+AdwaitPatel@users.noreply.github.com>
2025-07-22 19:49:46 +0530

    added server url to frontend

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // instance of axios with base URL
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://quizmakerapi.onrender.com",
 });
 
 export default api;
